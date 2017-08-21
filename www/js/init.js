d71f2b720ea31e336d99a33a82d1c5cc50d35da0
abdelwahed.farabi@esprit.tn <abdelwahed.farabi@esprit.tn>
2017-08-22 00:26:06 +0100

    user registration added

---
+++ www/js/init.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyCwEIWuZAMOSXgdsr1bKUA7SUMAnk4IC9s",
+  authDomain: "mystory-38a26.firebaseapp.com",
+  databaseURL: "https://mystory-38a26.firebaseio.com",
+  projectId: "mystory-38a26",
+  storageBucket: "mystory-38a26.appspot.com",
+  messagingSenderId: "612337990543"
+};
+
+  firebase.initializeApp(config);
